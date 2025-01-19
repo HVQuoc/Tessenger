2de4dc8077f14724385164f373c937b77df5864e
Quoc Hoang <hoangvanquoc923@gmail.com>
2025-01-19 15:32:20 +0700

    register api implementation

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoUrl = process.env.MONGO_URL;
+const jwtSecret = process.env.JWT_SECRET;
+mongoose.connect(mongoUrl);
+
+const app = express();
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+  })
+);
+app.use(express.json());
+
+const User = require("./models/User");
 
 app.get("/", (req, res) => {
-    res.json("ok")
-})
+  res.json("ok");
+});
 
-app.listen(4000)
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const createdUser = await User.create({ username, password });
+    jwt.sign({ userId: createdUser._id }, jwtSecret, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie("token", token).status(201).json({
+        id: createdUser._id,
+      });
+    });
+  } catch (err) {
+    if (err) throw err;
+    res.status(500).json("error");
+  }
+});
+
+app.listen(4000);
