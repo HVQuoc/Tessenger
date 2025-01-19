51e6c2f78481a8b362f333fc559519cf83c09112
Quoc Hoang <hoangvanquoc923@gmail.com>
2025-01-19 17:18:33 +0700

    build login route

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
-const cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcryptjs");
 dotenv.config();
 const mongoUrl = process.env.MONGO_URL;
 const jwtSecret = process.env.JWT_SECRET;
+const bcryptSalt = bcrypt.genSaltSync(10);
 mongoose.connect(mongoUrl);
 
 const app = express();
@@ -17,7 +19,7 @@ app.use(
   })
 );
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 const User = require("./models/User");
 
@@ -25,17 +27,50 @@ app.get("/", (req, res) => {
   res.json("ok");
 });
 
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const foundUser = await User.findOne({ username });
+  if (foundUser) {
+    const passOk = bcrypt.compareSync(password, foundUser.password);
+    if (passOk) {
+      jwt.sign(
+        { userId: foundUser._id, username: foundUser.username },
+        jwtSecret,
+        {},
+        (err, token) => {
+          res.cookie("token", token).json({
+            id: foundUser._id,
+          });
+        }
+      );
+    } else {
+      res.status(401).json("Bad credentials: invalid login information");
+    }
+  } else {
+    res.status(403).json({message: "Incorrect username or password"})
+  }
+});
+
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
 
   try {
-    const createdUser = await User.create({ username, password });
-    jwt.sign({ userId: createdUser._id, username }, jwtSecret, {}, (err, token) => {
-      if (err) throw err;
-      res.cookie("token", token).status(201).json({
-        id: createdUser._id,
-      });
+    const hashedPassword = bcrypt.hashSync(password, bcryptSalt);
+    const createdUser = await User.create({
+      username: username,
+      password: hashedPassword,
     });
+    jwt.sign(
+      { userId: createdUser._id, username },
+      jwtSecret,
+      {},
+      (err, token) => {
+        if (err) throw err;
+        res.cookie("token", token).status(201).json({
+          id: createdUser._id,
+        });
+      }
+    );
   } catch (err) {
     if (err) throw err;
     res.status(500).json("error");
@@ -50,7 +85,7 @@ app.get("/profile", (req, res) => {
       res.json(userData);
     });
   } else {
-    res.status(401).json("Unauthorized: no token")
+    res.status(401).json("Unauthorized: no token");
   }
 });
 
