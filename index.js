26049aa82c345ca8d3018e823fe468fe28961a83
Quoc Hoang <hoangvanquoc923@gmail.com>
2025-01-21 15:43:12 +0700

    add websocket server to api server

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const bcrypt = require("bcryptjs");
+const ws = require("ws");
 dotenv.config();
 const mongoUrl = process.env.MONGO_URL;
 const jwtSecret = process.env.JWT_SECRET;
@@ -47,7 +48,7 @@ app.post("/login", async (req, res) => {
       res.status(401).json("Bad credentials: invalid login information");
     }
   } else {
-    res.status(403).json({message: "Incorrect username or password"})
+    res.status(403).json({ message: "Incorrect username or password" });
   }
 });
 
@@ -89,4 +90,43 @@ app.get("/profile", (req, res) => {
   }
 });
 
-app.listen(4000);
+const server = app.listen(4000);
+
+// add socket server
+
+const wss = new ws.WebSocketServer({ server });
+wss.on("connection", (connection, req) => {
+  // console.log("connected", req.headers)
+  // connection.send('hello')
+
+  // grab the information of the connected client
+  const cookies = req.headers.cookie;
+  if (cookies) {
+    const tokenCookies = cookies
+      .split(";")
+      .find((str) => str.startsWith("token="));
+    if (tokenCookies) {
+      const token = tokenCookies.split("=")[1];
+      if (token) {
+        jwt.verify(token, jwtSecret, {}, (err, userData) => {
+          if (err) throw err;
+          const {userId, username} = userData;
+          connection.userId = userId;
+          connection.username = username;
+        })
+      }
+    }
+  }
+
+  // console.log([...wss.clients].map(c => c.username))
+
+  // send all active clients to all connected one
+  [...wss.clients].forEach(client => {
+    client.send(JSON.stringify(
+      {
+        online: [...wss.clients].map(c => ({userId: c.userId, username: c.username}))
+      }
+    ))
+  })
+
+});
